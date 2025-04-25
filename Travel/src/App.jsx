591b373a8d558018dd2a83fe7af26b0606ee0b14
Dhiraj Yadav <dhiraj.201708@ncit.edu.np>
2025-04-25 21:42:23 +0545

    add more functions with responsive

--- Travel/src/App.jsx
+++ Travel/src/App.jsx
@@ -1,35 +1,9 @@
 import Navbar from "./components/Navbar";
 import { useEffect } from "react";
-import { io } from "socket.io-client"; // Import Socket.IO client
 
-// Replace 'http://localhost:3000' with your Socket.IO server URL
-const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:3000");
 
 const App = () => {
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log("Socket.IO connection established");
-
-      // Send a test message to the server
-      socket.emit("testEvent", { type: "test", content: "Hello, server!" });
-    });
-
-    socket.on("pushNotification", (data) => {
-      console.log("Socket.IO message received:", data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Socket.IO connection closed");
-    });
-
-    socket.on("connect_error", (error) => {
-      console.error("Socket.IO connection error:", error);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+  
 
   return (
     <div className="">
