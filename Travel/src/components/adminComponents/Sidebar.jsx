e73c7801cd80c3c7feeceba7d5e92b38d8d057d3
Dhiraj Yadav <dhiraj.201708@ncit.edu.np>
2025-04-26 23:10:03 +0545

    ......

--- Travel/src/components/adminComponents/Sidebar.jsx
+++ Travel/src/components/adminComponents/Sidebar.jsx
@@ -29,99 +29,101 @@ function Sidebar() {
     
 
   return (
-    <div className='  h-screen flex flex-col bg-gray-800 '>
+    <div className='h-screen flex flex-col bg-gray-800'>
         <div className="text-center">
             <h1 className="font-bold text-blue-500 text-xl pt-5 pb-5 border-b-2 border-gray-700">Traveller's Mirror </h1>
-
         </div>
 
-        <div className="ml-6 mt-6 flex flex-col gap-4">
-            <p className="text-gray-400 text-md font-bold">DASHBOARD</p>
-            <div className="flex flex-col gap-4 items-start justify-start">
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaHome className='w-5 h-5 text-white'/>
-                    <Link to="/admin-dashboard" ><p className="text-gray-300 text-md ">Dashboard</p> </Link>
+        <div className="flex-1 overflow-y-auto"> 
+            {/* Sidebar content */}
+            <div className="ml-6 mt-6 flex flex-col gap-4">
+                <p className="text-gray-400 text-md font-bold">DASHBOARD</p>
+                <div className="flex flex-col gap-4 items-start justify-start">
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaHome className='w-5 h-5 text-white'/>
+                        <Link to="/admin-dashboard" ><p className="text-gray-300 text-md ">Dashboard</p> </Link>
 
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <BsFileEarmarkPost className='w-5 h-5 text-white'/>
-                    <Link to="/admin-add-blog" ><p className="text-gray-300 text-md ">Add Blog</p> </Link>
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <BsFileEarmarkPost className='w-5 h-5 text-white'/>
+                        <Link to="/admin-add-blog" ><p className="text-gray-300 text-md ">Add Blog</p> </Link>
+                        
+
+                    </div>
                     
 
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <TbLogs className='w-5 h-5 text-white'/>
+                        <Link to="/admin-posts" ><p className="text-gray-300 text-md ">Blogs</p> </Link>
+                        
+
+                    </div>
+
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <TbLogs className='w-5 h-5 text-white'/>
+                        <Link to="/admin-pending-posts" ><p className="text-gray-300 text-md ">Pending Blogs</p> </Link>
+                        
+
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <LuPackage className='w-5 h-5 text-white'/>
+                        <Link to="/admin-packages" ><p className="text-gray-300 text-md ">Packages</p> </Link>
+                        
+
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <LuPackage className='w-5 h-5 text-white'/>
+                        <Link to="/admin-add-package" ><p className="text-gray-300 text-md ">Add Package</p> </Link>
+                        
+
+                    </div>
+
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaComments className='w-5 h-5 text-white'/>
+                        
+                        <Link to="/admin-comments" ><p className="text-gray-300 text-md ">Comments</p> </Link>
+
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaPenNib className='w-5 h-5 text-white'/>
+                        <Link to="/admin-categories" ><p className="text-gray-300 text-md ">Categories</p> </Link>
+                        
+
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaUser className='w-5 h-5 text-white'/>
+                        <Link to="/admin-users" ><p className="text-gray-300 text-md ">Users</p> </Link>
+                        
+
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaUser className='w-5 h-5 text-white'/>
+                        <Link to="/admin-messages" ><p className="text-gray-300 text-md ">Messages</p> </Link>
+                        
+
+                    </div>
                 </div>
-                
+            </div>
 
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <TbLogs className='w-5 h-5 text-white'/>
-                    <Link to="/admin-posts" ><p className="text-gray-300 text-md ">Blogs</p> </Link>
-                    
 
-                </div>
+            <div className="ml-6 mt-10 mb-14 pb-10 flex flex-col gap-4">
+                <p className="text-gray-400 text-md font-bold">Advertise Management</p>
+                <div className="flex flex-col gap-4 items-start justify-start">
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaUser className='w-5 h-5 text-white'/>
+                        <Link to="/admin-add-advertise" ><p className="text-gray-300 text-md ">Add Advertise</p> </Link>
 
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <TbLogs className='w-5 h-5 text-white'/>
-                    <Link to="/admin-pending-posts" ><p className="text-gray-300 text-md ">Pending Blogs</p> </Link>
-                    
+                    </div>
+                    <div className="ml-6 flex items-center justify-start gap-3">
+                        <FaUser className='w-5 h-5 text-white'/>
+                        <Link to="/admin-add-hero" ><p className="text-gray-300 text-md ">Settings</p> </Link>
 
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <LuPackage className='w-5 h-5 text-white'/>
-                    <Link to="/admin-packages" ><p className="text-gray-300 text-md ">Packages</p> </Link>
-                    
-
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <LuPackage className='w-5 h-5 text-white'/>
-                    <Link to="/admin-add-package" ><p className="text-gray-300 text-md ">Add Package</p> </Link>
-                    
-
-                </div>
-
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaComments className='w-5 h-5 text-white'/>
-                    
-                    <Link to="/admin-comments" ><p className="text-gray-300 text-md ">Comments</p> </Link>
-
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaPenNib className='w-5 h-5 text-white'/>
-                    <Link to="/admin-categories" ><p className="text-gray-300 text-md ">Categories</p> </Link>
-                    
-
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaUser className='w-5 h-5 text-white'/>
-                    <Link to="/admin-users" ><p className="text-gray-300 text-md ">Users</p> </Link>
-                    
-
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaUser className='w-5 h-5 text-white'/>
-                    <Link to="/admin-messages" ><p className="text-gray-300 text-md ">Messages</p> </Link>
+                    </div>
                     
 
                 </div>
             </div>
         </div>
-
-
-        <div className="ml-6 mt-10 flex flex-col gap-4">
-            <p className="text-gray-400 text-md font-bold">Advertise Management</p>
-            <div className="flex flex-col gap-4 items-start justify-start">
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaUser className='w-5 h-5 text-white'/>
-                    <Link to="/admin-add-advertise" ><p className="text-gray-300 text-md ">Add Advertise</p> </Link>
-
-                </div>
-                <div className="ml-6 flex items-center justify-start gap-3">
-                    <FaUser className='w-5 h-5 text-white'/>
-                    <Link to="/admin-add-hero" ><p className="text-gray-300 text-md ">Settings</p> </Link>
-
-                </div>
-
-            </div>
-        </div>
-
 
         <div className="w-full pt-4 border-t-2 border-gray-500 flex items-center pl-6 absolute bottom-2 justify-between">
             <div className="flex items-center justify-between gap-3">
